188361651c14216d8adf4f864e6a00d90e8aa203
Reacubeth <644327005@qq.com>
2020-03-08 15:26:07 +0800

    Update KG Q&A System

--- KG-Search/Model/neo4j_models.py
+++ KG-Search/Model/neo4j_models.py
@@ -18,7 +18,7 @@ class Neo4jTool:
     def connect2neo4j(self):
         self.graph = Graph("http://localhost:7474", username="neo4j", password="123456")
 
-    def get_rel(self, val):
+    def format_loc(self, val):
         loc = val
         df = cpca.transform([loc], open_warning=False)
         sheng = ''
@@ -37,9 +37,88 @@ class Neo4jTool:
         loc = sheng + shi + qu
         if loc == '':
             loc = str(df['地址'][0])
-        sql = "MATCH (n1)<-[rel]-(n2:EVENT) WHERE n1.locationName=~'.*" + str(loc) + ".*' RETURN rel, n2 LIMIT 10;"
+        return loc
+
+    def get_rel(self, val):
+        loc = self.format_loc(val)
+        sql = "MATCH (n1)<-[rel]-(n2:EVENT) WHERE n1.locationName=~'.*" + str(loc) + ".*' RETURN n1, rel, n2 LIMIT 10;"
         answer = self.graph.run(sql).data()
-        sql2 = "MATCH (n1)<-[rel]-(n2:PATIENT) WHERE n1.locationName=~'.*" + str(loc) + ".*' RETURN rel, n2 LIMIT 20;"
+        sql2 = "MATCH (n1)<-[rel]-(n2:PATIENT) WHERE n1.locationName=~'.*" + str(loc) + ".*' RETURN n1, rel, n2 LIMIT 20;"
         answer.extend(self.graph.run(sql2).data())
-        print(sheng, shi, qu)
         return answer
+
+    def match_location4patient(self, val):
+        loc = self.format_loc(val)
+        sql = "MATCH (n1)<-[rel]-(n2:PATIENT) WHERE n1.locationName=~'.*" + str(
+            loc) + ".*' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_location4event_patient(self, val):
+        loc = self.format_loc(val)
+        sql = "MATCH (n1)<-[rel]-(n2:EVENT) WHERE n1.locationName=~'.*" + str(loc) + ".*' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        answer.extend(self.match_location4patient(loc))
+        return answer
+
+    def match_topic4event(self, val):
+        sql = "MATCH (n1:EVENT)-[rel:EVENT_TOPIC]->(n2:TOPIC) WHERE n2.topicName =~'.*" + str(val) + \
+              ".*' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_patient_name(self, val):
+        loc = self.format_loc(val)
+        sql = "MATCH (n1)-[rel]->(n2:EVENT) WHERE n1.patientName=~'.*" + str(loc) + ".*' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        # sql2 = "MATCH (n2:LOCATION)<-[rel]-(n1:PATIENT) WHERE n1.patientName=~'.*" + str(
+        #    loc) + ".*' RETURN n1, rel, n2 LIMIT 20;"
+        # answer.extend(self.graph.run(sql2).data())
+        return answer
+
+    def match_location_topic4event(self, loc, tpc):
+        loc = self.format_loc(loc)
+        sql = "MATCH (n1)<-[rel]-(n2:EVENT) WHERE n1.locationName=~'.*" + str(loc) + \
+              ".*' WITH n1, rel, n2 MATCH (n3)<-[rel2]-(n2:EVENT) WHERE n3.topicName=~'.*" + \
+              str(tpc) + ".*' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_location_gender4patient(self, loc, sex):
+        loc = self.format_loc(loc)
+        sql = "MATCH (n1)<-[rel]-(n2:PATIENT) WHERE n1.locationName=~'.*" + str(loc) + \
+              ".*' and n2.gender='" + str(sex) + "' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_location_time4event(self, loc, time):
+        loc = self.format_loc(loc)
+        sql = "MATCH (n1)<-[rel]-(n2:EVENT) WHERE n1.locationName=~'.*" + str(loc) + \
+              ".*' and n2.time = '" + str(time) + "' RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_location_time4event_patient(self, loc, time):
+        loc = self.format_loc(loc)
+        sql = "MATCH (n1)<-[rel]-(n2:EVENT) WHERE n1.locationName=~'.*" + str(loc) + \
+              ".*' and n2.time = '" + str(time) + \
+              "' WITH n1, rel, n2 MATCH (n3:PATIENT)-[rel2]->(n2) RETURN n1, rel, n2, rel2, n3 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_name_time4location_event(self, name, time):
+        sql = "MATCH (n1:PATIENT)-[rel]->(n2:EVENT) WHERE n1.patientName='" + str(name) + \
+              "' and n2.time='" + str(time) + \
+              "' WITH n1, rel, n2 MATCH (n2)-[rel2]-(n3:LOCATION) RETURN n1, rel, n2, rel2, n3;"
+        answer = self.graph.run(sql).data()
+        return answer
+
+    def match_location_time_topic4patient(self, loc, time, tpc):
+        loc = self.format_loc(loc)
+        sql = "MATCH (nx)<-[rel0]-(ny:EVENT) WHERE nx.topicName=~'.*" + str(tpc) + \
+              ".*' and ny.time='" + str(time) + \
+              "' WITH ny MATCH (n3)<-[rel2]-(ny) WHERE n3.locationName=~'.*" + str(loc) + \
+              ".*' WITH ny, n3 MATCH (n2:PATIENT)-[rel3]->(ny) WITH n2 MATCH (n1:LOCATION)<-[rel]-(n2) RETURN n1, rel, n2 LIMIT 40;"
+        answer = self.graph.run(sql).data()
+        return answer
+
--- KG-Search/web/entity.py
+++ KG-Search/web/entity.py
@@ -5,20 +5,107 @@
 
 from django.shortcuts import render
 from toolkit.initialization import neo_con
-
+from jieba import posseg
 import json
+import cpca
+
+
+def is_loc(loc):
+    d = cpca.transform([loc], open_warning=False)
+    if str(d['省'][0]):
+        return True
+    if str(d['市'][0]):
+        return True
+    if str(d['区'][0]):
+        return True
+    return False
+
+
+def entity_analysis(entity):
+    db = neo_con
+    words = entity.split(' ')
+    if len(words) == 1:
+        if is_loc(words[0]):
+            return db.match_location4event_patient(entity)
+        else:
+            wordp = posseg.cut(words[0])
+            for w in wordp:
+                if w.flag in ['v', 'vd', 'vn', 'vg']:
+                    return db.match_topic4event(entity)
+                elif w.flag in ['nr']:
+                    return db.match_patient_name(entity)
+    elif len(words) == 2:
+        isloc_dict = {}
+        flag = 0
+        for word in words:
+            isloc_dict[word] = is_loc(word)
+            if isloc_dict[word]:
+                flag = 1
+        if isloc_dict[words[0]]:
+            wordp = posseg.cut(words[1])
+            for w in wordp:
+                if w.flag in ['v', 'vd', 'vn', 'vg']:
+                    return db.match_location_topic4event(words[0], words[1])
+                elif w.flag in ['m']:
+                    return db.match_location_time4event_patient(words[0], words[1])
+                else:
+                    gender = words[1].replace('性', '').replace('生', '')
+                    return db.match_location_gender4patient(words[0], gender)
+        else:
+            wordp = posseg.cut(words[0])
+            for w in wordp:
+                if w.flag in ['v', 'vd', 'vn', 'vg']:
+                    return db.match_location_topic4event(words[1], words[0])
+                elif w.flag in ['m']:
+                    return db.match_location_time4event_patient(words[1], words[0])
+                else:
+                    gender = words[0].replace('性', '').replace('生', '')
+                    return db.match_location_gender4patient(words[1], gender)
+
+        if not flag:
+            wordp = posseg.cut(words[0])
+            for w in wordp:
+                if w.flag in ['m']:
+                    return db.match_name_time4location_event(words[1], words[0])
+                else:
+                    return db.match_name_time4location_event(words[0], words[1])
+    elif len(words) == 3:
+        loc = ''
+        time = ''
+        topic = ''
+        for word in words:
+            if is_loc(word):
+                loc = word
+                words.remove(word)
+                break
+        wordp = posseg.cut(words[0])
+        for w in wordp:
+            if w.flag in ['m']:
+                return db.match_location_time_topic4patient(loc, words[0],  words[1])
+            else:
+                return db.match_location_time_topic4patient(loc, words[1], words[0])
+
+    else:
+        answer = db.match_location4event_patient(words[0])
+        if len(answer) == 0:
+            answer = db.match_topic4event(words[0])
+        return answer
 
 
 def search_entity(request):
     nothing = {}
     if request.GET:
         entity = request.GET['user_text']
-        db = neo_con
-        entity_relation = db.get_rel(entity)
-        if len(entity_relation) == 0:
+        entity_relation = entity_analysis(entity)
+        try:
+            if len(entity_relation) == 0:
+                nothing = {'title': '<h1>Not Found</h1>'}
+                return render(request, 'entity.html', {'nothing': json.dumps(nothing, ensure_ascii=False)})
+            else:
+                return render(request, 'entity.html', {'entity_relation': json.dumps(entity_relation, ensure_ascii=False)})
+        except:
             nothing = {'title': '<h1>Not Found</h1>'}
             return render(request, 'entity.html', {'nothing': json.dumps(nothing, ensure_ascii=False)})
-        else:
-            return render(request, 'entity.html', {'entity_relation': json.dumps(entity_relation, ensure_ascii=False)})
 
     return render(request, "entity.html", {'nothing': nothing})
+
